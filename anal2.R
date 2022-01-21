ea42dd0a163b1141b80879e32b0cfe8903b4304b
Robin Hankin <hankin.robin@gmail.com>
2022-01-21 14:14:58 +1300

    more realistic profile

--- anal2.R
+++ anal2.R
@@ -47,20 +47,20 @@ colnames(sol_gravity) <- c("x","y","yd","ydd")
 
 
 sol_centrifugal <- data.frame(
-    ode(y=c(y=1,yd=0,ydd=1),       # initial conditions
+    ode(y=c(y=-1,yd=0,ydd=3),       # initial conditions
         times=seq(0,2,by=0.01),    # values on x-axis
         func=free_cord, parms=lf_centrifugal))
 colnames(sol_centrifugal) <- c("x","y","yd","ydd")
 
 
 sol_euler_a <- data.frame( # in green
-    ode(y=c(y=1,yd=0,ydd=1),       # initial conditions
+    ode(y=c(y=-1,yd=0,ydd=3),       # initial conditions
         times=seq(0,1,len=100),    # values on x-axis
         func=free_cord, parms=lf_euler(Omega=0.1)))
 colnames(sol_euler_a) <- c("x","y","yd","ydd")
 
 sol_euler_b <- data.frame( # in green
-    ode(y=c(y=1,yd=0,ydd=1),       # initial conditions
+    ode(y=c(y=-1,yd=0,ydd=3),       # initial conditions
         times=seq(0,1,len=100),    # values on x-axis
         func=free_cord, parms=lf_euler(Omega=-0.1)))
 colnames(sol_euler_b) <- c("x","y","yd","ydd")
@@ -71,7 +71,9 @@ sol_odd <- data.frame(
         func=free_cord, parms=lf_odd))
 colnames(sol_odd) <- c("x","y","yd","ydd")
 
-plot(NA,xlim=c(-1,1),ylim=c(1,3),asp=1)
+plot(NA,xlim=c(-1,1),ylim=c(-1,0),asp=1)
+
+
 if(FALSE){
     points(sol_gravity[,1:2],asp=1,col='black',ylim=c(0,4))
     points(-sol_gravity[,1],sol_gravity[,2],col='black')
